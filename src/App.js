0da2ef8e08d1935c1c53509c2e5150ecc99215b3
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-23 20:36:22 -0300

    Pegando dados da api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+import TmdbRequests from './API';
+
 function App() {
+
+  const [movieList, setMovieList] = useState('');
+
+  useEffect(() => {
+
+    (async () => {
+      let list = await TmdbRequests.getHomeList();
+      setMovieList(list);
+    })();
+
+  }, [])
+
   return (
     <>
-
+      {movieList !== '' && console.log(movieList)}
     </>
   );
 }
